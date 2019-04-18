dc18bb388e2d79e5d33a5e89c4d1534f739d76c3
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-04-18 14:47:52 -0400

    trying to commit from mac

--- src/stateless/UserGames.js
+++ src/stateless/UserGames.js
@@ -8,7 +8,7 @@ class UserGames extends Component {
     return (
     <ul id="user-positions">
         {this.props.userGames.map((game, index) => (
-          <p> {index}. {game} </p>
+          <button onClick={() => this.props.handleUserGameClick(game)} key={index}> Game {index+1} </button>
         ))}
 
     </ul>
